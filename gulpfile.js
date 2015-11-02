a8cde882e02048e977f76fe33d3e5c1d9b203085
nilptr <graychenchen@gmail.com>
2015-11-02 22:14:55 +0800

    gulpfile, duration

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,18 @@
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var csso = require('gulp-csso');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('build', function () {
+    gulp.src('src/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({}))
+        .pipe(gulp.dest('build'))
+        .pipe(csso())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['build']);
