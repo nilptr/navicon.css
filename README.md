365cd63b8c691fbf587b58ee1b1f800aaacfbff6
nilptr <graychenchen@gmail.com>
2016-01-12 17:50:22 +0800

    1. rename file & class 2. add banner 3. add README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# bars.css
+# navicon.css
+
+## Build
+
+```
+npm install --dev
+gulp
+```
+
+## Usage
+
+```html
+<div class="navicon">
+  <div class="bars"></div>
+</div>
+```
+
+根据情况修改代码,配合 js 动态修改相关的类即可
+
+## Demo
+
+[live Demo](http://static.bluegray.name/bluegray/demo/navicon.html)
--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,25 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var csso = require('gulp-csso');
 var rename = require('gulp-rename');
+var banner = require('gulp-banner');
 var autoprefixer = require('gulp-autoprefixer');
+
+var pkg = require('./package.json');
+
+var comment = '/*!\n' +
+    ' * <%= pkg.name %> <%= pkg.version %>\n' +
+    ' * <%= pkg.description %>\n' +
+    ' * <%= pkg.homepage %>\n' +
+    ' *\n' +
+    ' * Copyright 2016, <%= pkg.author %>\n' +
+    ' * Released under the <%= pkg.license %> license.\n' +
+    ' */\n';
 
 gulp.task('build', function () {
     gulp.src('src/*.scss')
         .pipe(sass())
         .pipe(autoprefixer({}))
+        .pipe(banner(comment, { pkg: pkg }))
         .pipe(gulp.dest('build'))
         .pipe(csso())
         .pipe(rename({ suffix: '.min' }))
